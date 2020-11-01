1a9205caed6b2080e8247c9a1399a3a62dc56e39
Clair.Huffine <Clair.huffine@colorado.edu>
2020-11-01 23:09:27 +0000

    added tests for volcano dataset

--- test_my_utils.py
+++ test_my_utils.py
@@ -14,7 +14,7 @@ class TestGetColumn(unittest.TestCase):
                                      'Boulder', 'cases')
         self.assertEqual(output[0:5], [1, 7, 7, 8, 8])
 
-    def testIntegerInput(self):
+    def test_IntegerInput(self):
         output = my_utils.get_column('data_testset.csv', 'county', 'Boulder',
                                      4)
         self.assertEqual(output[0:5], [1, 7, 7, 8, 8])
@@ -23,7 +23,7 @@ class TestGetColumn(unittest.TestCase):
         output = my_utils.get_column('data_testset.csv', 1, 'Boulder', 4)
         self.assertEqual(output[0:5], [1, 7, 7, 8, 8])
 
-    def testMultipleResultColumns(self):
+    def test_MultipleResultColumns(self):
         # test county query with integer input
         output = my_utils.get_column('data_testset.csv', 'county',
                                      'Boulder', [1, 2, 3])
@@ -36,7 +36,26 @@ class TestGetColumn(unittest.TestCase):
         correct = [['Boulder', 'Colorado', '08013']]*18
         self.assertListEqual(output, correct)
 
-    def testErrorModes(self):
+    def test_VolcanoDataSet(self):
+        # simple one line desired
+        output = my_utils.get_column('Aubry_2017_Table_S2_database_volcano_parameters.csv', 'Volcano',
+                                     'Calbuco', ['Latitude', 'Longitude'])
+        correct = [['-41.326', '-72.614']]
+        self.assertListEqual(output,correct)
+        
+        # find data past extra ',' in headerline
+        output = my_utils.get_column('Aubry_2017_Table_S2_database_volcano_parameters.csv', 'Volcano',
+                                     'Calbuco', 'Vent altitude (m a.s.l.)')
+        correct = [2003]
+        self.assertListEqual(output,correct)
+        
+        # multiple eruptions wanted
+        output = my_utils.get_column('Aubry_2017_Table_S2_database_volcano_parameters.csv', 'Volcano',
+                                     'Calbuco', 'Date start (UTC)')
+        correct = [['4/22/2015 21:04'], ['4/23/2015 3:54']]
+        self.assertListEqual(output,correct)
+
+    def test_ErrorModes(self):
         # file not found
         with self.assertRaises(SystemExit) as cm:
             my_utils.get_column('data_testset.cs', 2, 'Boulder', 5)
